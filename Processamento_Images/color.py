f8d4964e584bff9758bd80596f717b7bcd8fb25a
LUCAS HENRIQUE DE CARVALHO PINTO <lucas.hcpinto@senacsp.edu.br>
2022-08-23 21:30:54 -0300

    Ajustando arquivos

--- Processamento_Images/color.py
+++
@@ -1,8 +0,0 @@
-from PIL import ImageColor
-
-def pegaValorRGBA(color):
-    return ImageColor.getcolor(color, "RGBA")
-
-if __name__ == "__main__":
-    for color in ImageColor.colormap:
-        print(f"{color} = {pegaValorRGBA(color)}")
